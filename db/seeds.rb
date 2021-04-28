a12f26260a048eeee6057e47062f678ce40e3f65
Dani <dani8439@aol.com>
2021-04-28 10:20:50 -0400

    updated seeds again.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ Collection.destroy_all
 Piece.destroy_all 
 
 collection1 = Collection.create(name: "My Pieces", main_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm4HqcIYymJeTgpfIsVorAnyyRT6VPCspRpA&usqp=CAU")
-collection2 = Collection.create(name: "Wishlist", main_image: "https://i.pinimg.com/originals/67/36/e7/6736e7c53916465db8fe94c9a14e320e.jpg")
+collection2 = Collection.create(name: "Wishlist", main_image: "https://i.pinimg.com/originals/ec/0f/85/ec0f85979296b6aca4ebe24c24021a91.jpg")
 
 piece1 = Piece.create(piece_name: "3 Cup Teapot", pattern_name: "Utility", quantity: 2, image_url: "https://i.pinimg.com/originals/9b/8b/fa/9b8bfaea4e81c20ec817a1a63fb4cd17.jpg", collection_id: collection1.id)
 piece2 = Piece.create(piece_name: "Oval Platter", pattern_name: "Cambridge Blue Toast", quantity: 1, image_url: "https://i.pinimg.com/originals/ed/19/c9/ed19c95cfda3d385959c0d8a14eb53df.jpg", collection_id: collection2.id)
@@ -22,6 +22,6 @@ piece7 = Piece.create(piece_name: "3 Cup Teapot", pattern_name: "Cambridge Blue 
 piece8 = Piece.create(piece_name: "Butter Dish", pattern_name: "Utility", quantity: 1, image_url: "https://www.picclickimg.com/d/l400/pict/133529897128_/Emma-Bridgewater-Utility-Butter-Dish.jpg", collection_id: collection2.id)
 piece9 = Piece.create(piece_name: "Dollies Tea Set", pattern_name: "Tea Time Toast", quantity: 1, image_url: "https://i.pinimg.com/originals/87/db/82/87db821cc374578c33c87a94b6328772.jpg", collection_id: collection1.id)
 piece10 = Piece.create(piece_name: "Dollies Jug", pattern_name: "Black Toast", quantity: 1, image_url: "https://thumbs.worthpoint.com/zoom/images1/360/0914/13/emma-bridgewater-tiny-toast-marmalade_360_26fbb483517a5335967550bd40d906a6.jpg", collection_id: collection1.id)
-piece11 = Piece.create(piece_name: "Dollies teacup", pattern_name: "Black Toast", quantity: 2, image_url: "https://thumbs.worthpoint.com/zoom/images1/360/0512/31/emma-bridgewater-dollies-teacup_360_b8b3820477f7057e59f9813e7e3cec95.jpg", collection_id: collection2.id)
+piece11 = Piece.create(piece_name: "Dollies teacup", pattern_name: "Black Toast", quantity: 2, image_url: "https://i.ebayimg.com/thumbs/images/g/0eIAAOSwOCdfVjXE/s-l225.jpg", collection_id: collection2.id)
 piece12 = Piece.create(piece_name: "4 Cup Teapot", pattern_name: "Love & Kisses", quantity: 1, image_url: "https://i.pinimg.com/originals/03/ac/1c/03ac1c71a8a035c04eabcb7553e56284.jpg", collection_id: collection2.id)
 
